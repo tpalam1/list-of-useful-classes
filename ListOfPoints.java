b3e73d2ecd2b9e2a78b58ce1a55b6de019f33140
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2022-01-18 12:03:06 -0500

    Added ListOfPoints(ArrayList, ArrayList)
    Allows for splicing together separate lists of X and Y.

--- ListOfPoints.java
+++ ListOfPoints.java
@@ -15,6 +15,27 @@ public class ListOfPoints
     }
   } // creates a list of several instantiated points 
   
+  public ListOfPoints(ArrayList<Double> X_LIST, ArrayList<Double> Y_LIST) 
+    throws Exception 
+  {
+    int X_SIZE = X_LIST.size();
+    int Y_SIZE = Y_LIST.size();
+    
+    if(X_SIZE != Y_SIZE)
+    {
+      throw new Exception("ListOfPoints(X_LIST, Y_LIST): " + 
+        "lists are not of the same size.");
+    }
+    
+    for(int i = 0; i < X_SIZE; i++)
+    {
+      int curr_x = X_LIST.get(i);
+      int curr_y = Y_LIST.get(i);
+      
+      coords.add(new Point(curr_x, curr_y));
+    }
+  } // creates a list from splicing together an X_LIST and Y_LIST
+  
   // CONSTRUCTORS 
   
   public void add(Point p)
