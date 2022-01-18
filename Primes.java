ca40f0404b0e3fdd08221a1a000d14d5d2790366
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2022-01-17 23:09:58 -0500

    Added isCoprime(int, int)

--- Primes.java
+++ Primes.java
@@ -3,8 +3,8 @@ import java.util.Collections;
 
 public static Primes
 {
-	public static ArrayList<Integer> getPrimesBelow(int i)
-	{
+  public static ArrayList<Integer> getPrimesBelow(int i)
+  {
     ArrayList<Integer> output = new ArrayList<Integer>();
 	  
 	  for(int n = 1; n <= i; n++)
@@ -54,4 +54,35 @@ public static Primes
 	  return true;
 	  
 	} // checks if N is prime IF N is divisible by any [2, sqrt(N)]
+  
+	public static boolean isCoprime(int m, int n)
+	{
+	  ArrayList<Integer> FACTORS_N = getFactors(n);
+	  ArrayList<Integer> FACTORS_M = getFactors(m);
+	  
+	  for(int i = 0; i < FACTORS_N.size(); i++)
+	  {
+	    for(int j = 0; j < FACTORS_M.size(); j++)
+	    {
+	      Integer CURR_ENTRY_N = FACTORS_N.get(i); 
+	      Integer CURR_ENTRY_M = FACTORS_M.get(j);
+	      
+	      if(CURR_ENTRY_N.equals(1) || CURR_ENTRY_M.equals(1))
+	      {
+	        continue;
+	      } // continue if either current entry is 1 
+	      
+	      if(CURR_ENTRY_N.equals(CURR_ENTRY_M))
+	      {
+	        return false;
+	      } // break IF any factor is shared 
+	    }
+	  }
+	  
+	  // compares each factor of N and each factor of M; 
+	  // breaks loop if any are shared.
+	  
+	  return true; // null hypothesis 
+	} // returns TRUE IF N and M share any factors besides 1; FALSE elsewise. 
+
 } /** Class with various functions relating to prime numbers. **/
