f075da4210eb48cfce3b6492970f0415a619b7f9
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2022-01-29 15:48:54 -0500

    Added getStatus()
    Summarizes changes to the nominal inflation rate.

--- EconomicModels/MoneyMarket.java
+++ EconomicModels/MoneyMarket.java
@@ -20,4 +20,23 @@ public class MoneyMarket extends EconomicModel
   {
     this.y -= 10;
   }
+  
+  @Override
+  public String getStatus()
+  {
+    double change_in_NIR = this.y - 100;
+    
+    if(change_in_NIR == 0)
+    {
+      return "Running as usual.";
+    }
+    else if(change_in_NIR > 0)
+    {
+      return "Experiencing expansionary policy: nominal IR is higher than normal.";
+    }
+    else
+    {
+      return "Experiencing contractionary policy: nominal IR is lower than normal.";
+    }
+  }
 } /** Represents the money supply curves.**/
