c8d8d4ed1a053813346a8ee23b10d8b7f573604c
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2022-01-29 15:21:42 -0500

    Create LoanableFunds.java

---
+++ EconomicModels/LoanableFunds.java
@@ -0,0 +1,10 @@
+class LoanableFunds extends EconomicModel
+{
+  public LoanableFunds()
+  {
+    super();
+    
+    setAxisLabelX("REAL INTEREST RATE");
+    setAxisLabelY("QUANTITY OF LOANABLE FUNDS");
+  }
+} /** Represents the saving-borrowing curves. **/
