352fef0139b8f5bc42d7287eeea775813ecd6991
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2022-03-27 22:37:47 -0400

    Update InfoDistribution.java
    Rewrote the whole class + added the driver class.

--- Misc./InfoDistribution.java
+++ Misc./InfoDistribution.java
@@ -1,11 +1,109 @@
 import java.util.ArrayList;
 
-public class InfoDistribution
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	  double area_threshold = 0.50;
+	  
+	  for(int i = 0; i < 1000; i++)
+	  {
+  	  InfoDistribution id = new InfoDistribution("RANDCUMUL");
+  	  ArrayList<Double> arr = getOptimalStrategy(id, area_threshold);
+  	  
+  	  System.out.println(arr.get(1) + "\t" + arr.get(2)
+  	  + "\t" + arr.get(3) + "\t" + arr.get(4) + "\t" + arr.get(5));
+	  }
+	}
+	
+	public static ArrayList<Double> getOptimalStrategy(InfoDistribution id, double area_threshold)
+	{
+	  ArrayList<Double> output = new ArrayList<Double>();
+	  
+	  double min_read_i = 1e3; // we are trying to minimize this 
+	  double best_start = 0;
+	  double best_end = 0;
+    
+    for(double i = 0; i <= 1; i += 0.001)
+    {
+      double i2 = getCoresspondingX(id, i, area_threshold);
+      double total_i = i + i2; // current total amount to have been read 
+      
+      // System.out.println(fix(i) + "\t" + fix(i2) + "\t" + fix((i + i2)));
+      
+      if(total_i < min_read_i)
+      {
+        min_read_i = total_i;
+        best_start = i;
+        best_end = i2;
+      }
+    }
+    
+    output.add(fix(min_read_i));
+    output.add(fix(best_start));
+    output.add(fix(best_end));
+    // stores the results 
+    
+	  output.add(fix(id.y.get(0)));
+  	output.add(fix(id.y.get(1)));
+  	output.add(fix(id.y.get(2)));
+  	// stores the coefficients used to achieve the results 
+    
+    return output;
+	} 
+	/**
+	 * Returns (total_read, x1, x2), such (0, x1)U(1-x2, 1) = area_threshold,
+	 * and total_read is minimized. Last 3 vars represent coefficients used to achieve the results.
+	 **/
+	
+	public static double getCoresspondingX(InfoDistribution id, double x1, double area_threshold)
+	{
+    double area_1 = id.getAreaBetween(0, x1); // area from (0, x1)
+    
+    for(double i = 0; i <= (1-x1); i += 0.01)
+    {
+      double area_2 = id.getAreaBetween(1-i, 1);
+      
+      double area_total = area_1 + area_2;
+      
+      if(area_total > area_threshold)
+      {
+        return i;
+      }
+    }
+    
+    return -1; // bad output 
+	} // finds X2 SUCH (0, x1)U(1-x2, 1) = area_threshold.
+	
+  public static double getRandDouble(double min, double max)
+  {
+    return Math.random() * (max - min) + min;
+  } // returns a random double, [min, max)
+  
+  public static double fix(double d)
+  {
+    d = Math.floor(d * 1000) / 1000;
+    
+    return d;
+  } // sets a given Double to 3 decimal precision 
+  
+  public static void fix(ArrayList<Double> arr)
+  {
+    for(int i = 0; i < arr.size(); i++)
+    {
+      double curr_val = arr.get(i);
+      arr.set(i, fix(curr_val));
+    }
+  } // fixes all values in an ArrayList to 3 decimal precision 
+} /** Driver class that finds the optimal (first, last) reading strategy of a book. **/
+
+
+class InfoDistribution
 {
   ArrayList<Double> x = new ArrayList<Double>();
   ArrayList<Double> y = new ArrayList<Double>();
   
-  String mode; 
+  String mode = ""; 
 	
 	public InfoDistribution()
 	{
@@ -24,8 +122,9 @@ public class InfoDistribution
 	
   public static double getRandDouble(double min, double max)
   {
-    return Math.random() * (max - min + 1) + min;
-  } // returns a random double, [min, max]
+    
+    return Math.random() * (max - min) + min;
+  } // returns a random double, [min, max)
 	
 	public void fillX()
 	{
@@ -46,12 +145,12 @@ public class InfoDistribution
 	    case "RANDNORMAL":
 	      y1 = getRandDouble(0, 100);
 	      y3 = getRandDouble(0, y1);
-	      y2 = getRandDouble(y1, y3);
+	      y2 = getRandDouble(0, y3);
 	      break;
 	    case "RANDCUMUL":
 	      y1 = getRandDouble(0, 100);
 	      y2 = getRandDouble(0, y1);
-	      y3 = getRandDouble(0, y3);
+	      y3 = getRandDouble(0, y2);
 	      break;
 	    case "RANDOM":
       default:
