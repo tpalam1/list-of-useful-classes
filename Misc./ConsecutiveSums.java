c9f3b58720548c21492e07a44254d52cfdf6dce8
tpalam1 <70032663+tpalam1@users.noreply.github.com>
2022-05-22 22:58:32 -0400

    Create ConsecutiveSums.java
    Inspired by Parker, Matt. Totally addicted to base. In: Things to make and do in the fourth dimension. Strauss and Giroux Publishing. 2014. 
    
    A number is said to be a consequential number (own terminology) if it can be composed of sums of consecutive integers; for example:
    * 9 = 4 + 5
    * 11 = 5 + 6
    * 12 = 3 + 4 + 5
    
    Using this concept of consequential numbers, I tried to find how to compose any number N using the largest string of consecutive numbers. 
    
    Discoveries:
    * All numbers who cannot be composed this way appear to be powers of 2. 
    * All numbers who need a 0 at the start of the chain to be composed of the largest amount of consecutive integers: 6, 10, 28, 136, 496...
    * 50 = sum of all numbers from [-10, 25].
    * 100 = sum of all numbers from [-17, 40].
    * 500 = sum of all numbers from [-97, 200].
    * 1 000 = sum of all numbers from [-197, 400]. Appears strangely related to the N = 500 case.

---
+++ Misc./ConsecutiveSums.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	  System.out.println("N\tSTARTING VALUE\tNUMBER OF CONSECUTIVE INTEGERS\tVERDICT");
+		for(int i = 3; i <= 1000; i++)
+		{
+		  ArrayList<Double> results = getLargestComposition(i);
+		  
+		  double A = results.get(0);
+		  double B = results.get(1);
+		  
+		  String verdict = getVerdict(A, B);
+		  
+		  System.out.println(i + "\t" + A + "\t\t" + B + "\t" + verdict);
+		}
+	}
+	
+	public static double getA(int num_consect_values)
+	{
+	  return 0.5 * (num_consect_values * num_consect_values - num_consect_values);
+	}
+	
+	public static double getStartingValueOfConsecutiveDecomposition(int num, int num_consect_values)
+	{
+	  double A = getA(num_consect_values);
+	  double B = num_consect_values;
+	  
+	  return (num - A) / B;
+	}
+	
+	public static boolean isComposable(int num, int num_consect_values)
+	{
+	  double a = getStartingValueOfConsecutiveDecomposition(num, num_consect_values);
+	  
+	  if(isInteger(a))
+	  {
+	    return true;
+	  }
+	  return false;
+	}
+	
+	public static boolean isInteger(double num)
+	{
+	  if(num % 1 == 0)
+	  {
+	    return true;
+	  }
+	  return false;
+	}
+	
+	public static ArrayList<Double> getLargestComposition(int num)
+	{
+	  double starting_value = -1;
+	  double num_consect_values = -1; 
+	  // null return in case the loop returns nothing 
+	  
+	  for(int i = 2; i < num; i++)
+	  {
+	    if(isComposable(num, i))
+	    {
+	      starting_value = getStartingValueOfConsecutiveDecomposition(num, i);
+	      num_consect_values = i;
+	      
+	      // System.out.println(starting_value + "\t" + num_consect_values);
+	    }
+	  }
+	  
+	  ArrayList<Double> output = new ArrayList<Double>();
+	  output.add((double)starting_value);
+	  output.add(num_consect_values);
+	  // returns the output as a (starting_value, number of consec. values pair)
+	  
+	  return output;
+	}
+	
+	public static String getVerdict(double A, double B)
+	{
+	  String verdict = "";
+	  
+    if((A == -1) && (B == -1))
+    {
+      verdict = "IMPOSSIBLE";
+    }
+    else if((A == 0))
+    {
+      verdict = "POSSIBLE, IF STARTING WITH ZERO";
+    }
+    else if((A < 0))
+    {
+      verdict = "POSSIBLE, IF STARTING WITH NEGATIVE NUMBERS";
+    }
+    else 
+    {
+      verdict = "--";
+    }
+    
+    return verdict;
+	}
+}
